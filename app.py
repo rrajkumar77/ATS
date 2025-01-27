ef589928520a2aaa14cbb1023278e513384a2e4c
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2025-01-27 15:07:01 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_gemini_response(input):
     response = model.generate_content(input)
     return response.text
 
-#convert pdf to text
+# Convert PDF to text
 def input_pdf_text(uploaded_file):
     reader=pdf.PdfReader(uploaded_file)
     text=""
@@ -23,19 +23,13 @@ def input_pdf_text(uploaded_file):
 
 input_prompt ="""
 
-### As a skilled ATS with advanced technology and domain knowledge, meticulously evaluate a candidate's resume based on the provided job description by:
-Calculating the match percentage between the resume and job description, providing a number and explanation.
-Identifying missing key keywords from the resume compared to the job description.
-Offering specific, actionable tips to enhance the resume and align it with job requirements.
-Creating a table listing skills, years of experience, and relevant projects.
-
 ### As a skilled ATS with advanced technology and domain knowledge, your role is to meticulously evaluate a candidate's resume based on the provided job description by:
 Analyzing the resume for relevant skills, experiences, and qualifications that align with the job requirements. Look for key buzzwords and specific criteria outlined in the job description to determine the candidate's suitability for the position.
 Calculating the match percentage between the resume and job description, providing a number and explanation.
 Identifying missing key keywords from the resume compared to the job description.
 Offering specific, actionable tips to enhance the resume and align it with job requirements.
 Creating a table listing skills, years of experience, and relevant projects.
-Your evaluation should be thorough, precise, and objective, ensuring that the most qualified candidates are accurately identified based on their resume content in relation to the job criteria.
+Your evaluation should be thorough, precise, and objective. It should ensure that the most qualified candidates are accurately identified based on their resume content concerning the job criteria.
 Provide a detailed assessment of how well the resume matches the job requirements, highlighting strengths, weaknesses, and any potential areas of concern. Offer constructive feedback on how the candidate can enhance their resume to better align with the job description and improve their chances of securing the position.
 Remember to utilize your expertise in technology and data science to conduct a comprehensive evaluation that optimizes the recruitment process for the hiring company. Your insights will play a crucial role in determining the candidate's compatibility with the job role.
 
@@ -45,12 +39,11 @@ jd={jd}
 1. Calculate the match percentage between the resume and job description, providing a number 
 2. Explain the match.
 3. Identify missing keywords or skills from the resume compared to the job description.
-4. Create a table listing top skills, required years of experience (JD), candidate's years of experience (Resume), and relevant projects.
+4. Create a table that includes the top 5 skills, the required years of experience (JD), the candidate's years of experience (Resume), and the relevant projects they have worked on.
 """
 
 ##streamlit
-
-st.title("Raj Smart ATS")
+st.title("Raj's Smart ATS")
 st.text("Imporve your ATS resume score Match")
 jd = st.text_area("Paste job description here")
 uploaded_file= st.file_uploader("Upload your resume", type="pdf", help= "Please upload the pdf")
