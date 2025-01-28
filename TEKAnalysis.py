d5c1fe7ca431cd8fcb3b602f0320ad0dd0436bda
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2025-01-28 10:32:01 +0530

    Add files via upload

---
+++ TEKAnalysis.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+load_dotenv()
+import fitz 
+
+import google.generativeai as genai
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input, pdf_content, prompt):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content([input, pdf_content, prompt])
+    return response.text
+
+def input_pdf_setup(uploaded_file):
+    if uploaded_file is not None:
+        # Read the PDF file
+        document = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+        # Initialize a list to hold the text of each page
+        text_parts = []
+
+        # Iterate over the pages of the PDF to extract the text
+        for page in document:
+            text_parts.append(page.get_text())
+
+        # Concatenate the list into a single string with a space in between each part
+        pdf_text_content = " ".join(text_parts)
+        return pdf_text_content
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+## Streamlit App
+
+st.set_page_config(page_title="Resume Expert")
+
+st.header("JobFit Analyzer")
+st.subheader('This Application helps you to evaluate the Resume Review with the Job Description')
+input_text = st.text_input("Job Description: ", key="input")
+uploaded_file = st.file_uploader("Upload your Resume(PDF)...", type=["pdf"])
+pdf_content = ""
+
+if uploaded_file is not None:
+    st.write("PDF Uploaded Successfully")
+
+submit1 = st.button("Technical Recruiter Analysis")
+
+submit2 = st.button("Account Manager Analysis")
+
+submit3 = st.button("Domain Expert Analysis")
+
+submit4 = st.button("Technical Manager Analysis")
+
+input_promp = st.text_input("Queries: Feel Free to Ask here")
+
+submit5 = st.button("Answer My Query")
+
+input_prompt1 = """
+Role: Experienced Technical Human Resource Manager with expertise in technical evaluations and Recruitement
+Task: Review the provided resume against the job description.
+Objective: Evaluate whether the candidate's profile aligns with the role.
+Instructions:
+Provide a professional evaluation of the candidate's profile.
+Highlight the strengths and weaknesses of the applicant concerning the specified job requirements.
+"""
+
+input_prompt2 = """
+Role: Experienced Technical Human Resource Manager with expertise in technical evaluations
+Task: Scrutinize the provided resume in light of the job description.
+Objective: Evaluate the candidate's suitability for the role from an HR perspective.
+Instructions:
+Share insights on the candidate's suitability for the role.
+Highlight the strengths and weaknesses of the applicant concerning the job requirements.
+Identify areas where improvement is needed.
+Offer advice on enhancing the candidate's skills.
+"""
+
+input_prompt3 = """
+Role: Skilled ATS (Applicant Tracking System) scanner with expertise in domain and ATS functionality
+Task: Evaluate the provided resume against the job description.
+Objective: Assess the compatibility of the resume with the job description from a Domain Expert perspective. (Eg: Business Analyst(BA), Functional Manger or Project Manager)
+Instructions:
+Calculating the match percentage between the resume and job description, provide a percentage number and explanation.
+Identify any missing keywords in the resume relevant to the job description.
+Your evaluation should be thorough, precise, and objective. It should ensure that the most qualified candidates are accurately identified based on their resume content concerning the job criteria.
+"""
+input_prompt4 = """
+Role: Skilled ATS (Applicant Tracking System) scanner with a deep understanding of the technology and Technical skills mentioned in the job description and ATS functionality
+Task: Evaluate the provided resume against the job description.
+Objective: Assess the compatibility of the resume with the job description from a Technical Expert perspective.
+Instructions:
+1. Calculate the match percentage between the resume and job description, provide a percentage number 
+2. Explain the match and the gap
+3. Identify missing keywords or skills from the resume compared to the job description.
+4. Create a table that includes the top 5 skills, the required years of experience (JD), the candidate's years of experience (Resume), and the relevant projects they have worked on.
+5. Share final thoughts on the candidate's suitability for the role.
+
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt1, pdf_content, input_text)
+        st.subheader("Technical Recruiter Analysis")
+        st.write(response)
+    else:
+        st.write("Please upload a PDF file to proceed.")
+
+elif submit2:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt2, pdf_content, input_text)
+        st.subheader("Account Manager Analysis")
+        st.write(response)
+    else:
+        st.write("Please upload a PDF file to proceed.")
+
+elif submit3:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt3, pdf_content, input_text)
+        st.subheader("Domain Expert Analysis")
+        st.write(response)
+    else:
+        st.write("Please upload a PDF file to proceed.")
+
+elif submit4:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_prompt4, pdf_content, input_text)
+        st.subheader("Technical Manager Analysis")
+        st.write(response)
+    else:
+        st.write("Please upload a PDF file to proceed.")
+
+elif submit5:
+    if uploaded_file is not None:
+        pdf_content = input_pdf_setup(uploaded_file)
+        response = get_gemini_response(input_promp5, pdf_content, input_text)
+        st.subheader("The Response is")
+        st.write(response)
+    else:
+        st.write("Please upload a PDF file to proceed.")
