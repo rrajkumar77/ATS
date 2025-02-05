0fffed24c3d525f35ca454699a087709cad11c1e
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2025-02-05 15:34:38 +0530

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -126,27 +126,6 @@ Finally, validate that my resume is saved in a compatible format (e.g., .docx or
 8. Skills Comparison Table: Create a table listing the skills required in the job description and how the candidate’s skills align with them.  
 """
 
-'''
-input_prompt6 = """
-Role: Skilled ATS (Applicant Tracking System) scanner with expertise in domain-specific ATS functionality.  
-Task: Evaluate the provided resume against the given job description.  
-Objective: Assess the compatibility of the resume with the job description from a Human Resource manager's perspective.  
-Instructions:  
-1. Keyword Analysis: Identify missing keywords in the resume that are relevant to the job description.  
-2. Skill Enhancement: Provide recommendations for improving the candidate’s skillset based on the job requirements.  
-3. Development Areas: Identify areas where the candidate needs further development to better match the role.  
-4. Cover Letter Drafting: Write a compelling cover letter that effectively demonstrates how the skills listed in the resume align with the job requirements.  
-5. Skills Comparison Table: Create a table that lists the skills required in the job description alongside how the candidate’s skills align with them.  
-6. Resume Optimization: Modify and enhance the resume to ensure it aligns with the job description, optimizing it for Applicant Tracking System (ATS) compatibility while maintaining a professional format.  
-Output Format:  
-- A detailed list of missing keywords.  
-- Specific skill enhancement recommendations.  
-- A concise analysis of areas requiring improvement.  
-- A professionally written cover letter.  
-- A structured comparison table matching job description skills with resume skills.  
-- An ATS-optimized version of the resume.  
-"""
-'''
 
 if submit1:
     if uploaded_file is not None:
