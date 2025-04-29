e568d7738a204ccec9c2bb5e507d1ca57b632b3f
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2025-04-29 15:44:55 +0530

    Update Project_Update1.py

--- Project_Update1.py
+++ Project_Update1.py
@@ -9,19 +9,18 @@ def extract_project_updates(uploaded_file):
     
     formatted_updates = []
     for index, row in project_updates.iterrows():
-        formatted_update = f"""
-        **Employee Name**: {row['Created By']}
-        **Lead Name**: {row['Team_Lead']}
-        **Project Name**: {row['Project_Name']}
-        **Project Description**: {row['Project_Description']}
-        **Achievements/Value Adds**:
-        - {row['Acheivements_ValueAdds'].replace(';', '\n- ')}
-        **Value Add**:
-        - {row['Value_Add'].replace(';', '\n- ')}
-        """
+        formatted_update = {
+            "Employee Name": row['Created By'],
+            "Lead Name": row['Team_Lead'],
+            "Project Name": row['Project_Name'],
+            "Project Description": row['Project_Description'],
+            "Achievements/Value Adds": row['Acheivements_ValueAdds'].replace(';', '.\n- '),
+            "Value Add": row['Value_Add'].replace(';', '.\n- ')
+        }
         formatted_updates.append(formatted_update)
     
-    return formatted_updates
+    formatted_df = pd.DataFrame(formatted_updates)
+    return formatted_df
 
 ## Streamlit App
 
@@ -35,5 +34,4 @@ if uploaded_file is not None:
     st.write("Document Uploaded Successfully")
     project_updates = extract_project_updates(uploaded_file)
     st.subheader("Project Updates")
-    for update in project_updates:
-        st.write(update)
+    st.write(project_updates)
