d95105a294151432a88b6e5444c971e75a63cd99
R Rajkumar <46582310+rrajkumar77@users.noreply.github.com>
2025-10-06 14:45:55 +0530

    Update JD_Resume_Analyser.py

--- JD_Resume_Analyser.py
+++ JD_Resume_Analyser.py
@@ -16,7 +16,7 @@ os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 #def get_gemini_response(input, pdf_content, prompt):
- #   model = genai.GenerativeModel('gemini-1.5-flash')
+ #   model = genai.GenerativeModel('gemini-2.0-flash')
   #  response = model.generate_content([input, pdf_content, prompt])
    # return response.text
 
@@ -261,3 +261,4 @@ elif submit7:
         st.write(response)
     else:
         st.write("Please upload a Job Description to proceed.")
+
